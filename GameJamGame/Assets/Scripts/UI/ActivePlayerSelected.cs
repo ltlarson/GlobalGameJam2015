422c966a02920a7497a2d0fac988f0a9c7008e9d
Luke <larson.luke.t@gmail.com>
2015-01-24 04:03:22 -0600

    EVERYTHING IS SAVED
    Uploading the build.

---
+++ GameJamGame/Assets/Scripts/UI/ActivePlayerSelected.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum CharacterSelected { Red,Green,Blue};
+public class ActivePlayerSelected : MonoBehaviour {
+    public Image Arrow;
+    
+    public CharacterSelected myCharacter;
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Arrow.GetComponent<Animator>().SetInteger("ControlInt", (int)myCharacter);
+	}
+
+    public void SetActive(CharacterSelected input)
+    {
+        myCharacter = input;
+    }
+
+}
